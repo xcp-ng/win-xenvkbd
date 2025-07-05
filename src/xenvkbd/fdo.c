xcp-ng/win-xenvkbd

commit 270cb87d3d2a50e8788add7ed6a1ca042173be38

Invalidate FDOs when no devices are present
In cases where the entire xenstore device key is removed, FdoScan()
currently skips reenumeration entirely. This causes it to not pass the
removal event to xennet. Fix the issue by reenumerating devices even if
xenstore key does not exist.

Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

Ported from XENVIF.

Signed-off-by: Paul Durrant <pdurrant@amazon.com>

src/xenvkbd/fdo.c
@@ -756,36 +756,38 @@ __FdoEnumerate(
             Name = PdoGetName(Pdo);
             Missing = TRUE;
 
-            // If the PDO already exists and its name is in the device list
-            // then we don't want to remove it.
-            for (Index = 0; Devices[Index].Buffer != NULL; Index++) {
-                PANSI_STRING Device = &Devices[Index];
+            if (Devices != NULL) {
+                // If the PDO already exists and its name is in the device list
+                // then we don't want to remove it.
+                for (Index = 0; Devices[Index].Buffer != NULL; Index++) {
+                    PANSI_STRING Device = &Devices[Index];
 
-                if (Device->Length == 0)
-                    continue;
+                    if (Device->Length == 0)
+                        continue;
 
-                if (strcmp(Name, Device->Buffer) == 0) {
-                    Missing = FALSE;
-                    Device->Length = 0;  // avoid duplication
-                    break;
+                    if (strcmp(Name, Device->Buffer) == 0) {
+                        Missing = FALSE;
+                        Device->Length = 0;  // avoid duplication
+                        break;
+                    }
                 }
-            }
 
-            if (!PdoIsMissing(Pdo)) {
-                if (PdoIsEjectRequested(Pdo)) {
-                    IoRequestDeviceEject(PdoGetDeviceObject(Pdo));
-                } else if (Missing) {
-                    PdoSetMissing(Pdo, "device disappeared");
+                if (!PdoIsMissing(Pdo)) {
+                    if (PdoIsEjectRequested(Pdo)) {
+                        IoRequestDeviceEject(PdoGetDeviceObject(Pdo));
+                    } else if (Missing) {
+                        PdoSetMissing(Pdo, "device disappeared");
 
-                    // If the PDO has not yet been enumerated then we can
-                    // go ahead and mark it as deleted, otherwise we need
-                    // to notify PnP manager and wait for the REMOVE_DEVICE
-                    // IRP.
-                    if (PdoGetDevicePnpState(Pdo) == Present) {
-                        PdoSetDevicePnpState(Pdo, Deleted);
-                        PdoDestroy(Pdo);
-                    } else {
-                        NeedInvalidate = TRUE;
+                        // If the PDO has not yet been enumerated then we can
+                        // go ahead and mark it as deleted, otherwise we need
+                        // to notify PnP manager and wait for the REMOVE_DEVICE
+                        // IRP.
+                        if (PdoGetDevicePnpState(Pdo) == Present) {
+                            PdoSetDevicePnpState(Pdo, Deleted);
+                            PdoDestroy(Pdo);
+                        } else {
+                            NeedInvalidate = TRUE;
+                        }
                     }
                 }
             }
@@ -795,15 +797,17 @@ __FdoEnumerate(
     }
 
     // Walk the class list and create PDOs for any new device
-    for (Index = 0; Devices[Index].Buffer != NULL; Index++) {
-        PANSI_STRING Device = &Devices[Index];
+    if (Devices != NULL) {
+        for (Index = 0; Devices[Index].Buffer != NULL; Index++) {
+            PANSI_STRING Device = &Devices[Index];
 
-        if (Device->Length == 0)
-            continue;
+            if (Device->Length == 0)
+                continue;
 
-        status = PdoCreate(Fdo, Device);
-        if (NT_SUCCESS(status))
-            NeedInvalidate = TRUE;
+            status = PdoCreate(Fdo, Device);
+            if (NT_SUCCESS(status))
+                NeedInvalidate = TRUE;
+        }
     }
 
     __FdoReleaseMutex(Fdo);
@@ -950,8 +954,12 @@ FdoScan(
             Devices = NULL;
         }
 
-        if (Devices == NULL)
-            goto loop;
+        if (Devices == NULL) {
+            if (status == STATUS_OBJECT_PATH_NOT_FOUND)
+                goto invalidate;
+            else
+                goto loop;
+        }
 
         if (ParametersKey != NULL) {
             status = RegistryQuerySzValue(ParametersKey,
@@ -991,9 +999,11 @@ FdoScan(
         if (UnsupportedDevices != NULL)
             RegistryFreeSzValue(UnsupportedDevices);
 
+invalidate:
         NeedInvalidate = __FdoEnumerate(Fdo, Devices);
 
-        __FdoFreeAnsi(Devices);
+        if (Devices != NULL)
+            __FdoFreeAnsi(Devices);
 
         if (NeedInvalidate) {
             NeedInvalidate = FALSE;

commit 0fee77a974360265faf521b4f4192cf697b8fe62

Fix FDO invalidation when no devices are present
__FdoEnumerate is called with Devices==NULL to signify that the entire
device xenstore key has been removed. However, the PdoIsMissing check
was under the Devices!=NULL check, and will never be called. This means
existing PDOs will never be validated in this situation.

Instead, always check for the PDO's existence even if Devices==NULL.

Fixes: 270cb87d3d2a ("Invalidate FDOs when no devices are present")
Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

src/xenvkbd/fdo.c
@@ -771,23 +771,23 @@ __FdoEnumerate(
                         break;
                     }
                 }
+            }
 
-                if (!PdoIsMissing(Pdo)) {
-                    if (PdoIsEjectRequested(Pdo)) {
-                        IoRequestDeviceEject(PdoGetDeviceObject(Pdo));
-                    } else if (Missing) {
-                        PdoSetMissing(Pdo, "device disappeared");
+            if (!PdoIsMissing(Pdo)) {
+                if (PdoIsEjectRequested(Pdo)) {
+                    IoRequestDeviceEject(PdoGetDeviceObject(Pdo));
+                } else if (Missing) {
+                    PdoSetMissing(Pdo, "device disappeared");
 
-                        // If the PDO has not yet been enumerated then we can
-                        // go ahead and mark it as deleted, otherwise we need
-                        // to notify PnP manager and wait for the REMOVE_DEVICE
-                        // IRP.
-                        if (PdoGetDevicePnpState(Pdo) == Present) {
-                            PdoSetDevicePnpState(Pdo, Deleted);
-                            PdoDestroy(Pdo);
-                        } else {
-                            NeedInvalidate = TRUE;
-                        }
+                    // If the PDO has not yet been enumerated then we can
+                    // go ahead and mark it as deleted, otherwise we need
+                    // to notify PnP manager and wait for the REMOVE_DEVICE
+                    // IRP.
+                    if (PdoGetDevicePnpState(Pdo) == Present) {
+                        PdoSetDevicePnpState(Pdo, Deleted);
+                        PdoDestroy(Pdo);
+                    } else {
+                        NeedInvalidate = TRUE;
                     }
                 }
             }